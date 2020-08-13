72d7e931148ffcc3131ed46454c13d8288a82b72
ltkum <kunzi.martin@gmail.com>
2020-08-13 14:22:55 +0200

    function to create the json response

---
+++ app/helpers/controllers.py
@@ -0,0 +1,5 @@
+def create_response(base_url, short_id, error_message=None):
+    response = {"shorturl" : base_url + short_id}
+    if error_message is not None:
+        response["error"] = error_message
+    return response
