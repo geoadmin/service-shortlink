0e9f4118b0a805caadd03a123ed81fd33f6fe4a7
Martin Künzi <kunzi.martin@gmail.com>
2020-10-30 12:00:37 +0100

    solved scope issue in tests, correction to the table creation

--- tests/unit_tests/test_helpers.py
+++ tests/unit_tests/test_helpers.py
@@ -5,13 +5,27 @@ import unittest
 import os
 import time
 from moto import mock_dynamodb2
+from boto3.dynamodb.conditions import Key
+
 import logging.config
 logger = logging.getLogger(__name__)
 
+setup_already_made = 0
+
 
 class TestDynamoDb(unittest.TestCase):
+
     @mock_dynamodb2
     def setup(self):
+        self.valid_urls_list = [
+            "https://map.geo.admin.ch/?lang=fr&topic=ech&bgLayer=ch.swisstopo.pixelkarte-farbe&layers=ch.swisstopo.zeitreihen,ch.bfs.gebaeude_wohnungs_register,ch.bav.haltestellen-oev,ch.swisstopo.swisstlm3d-wanderwege&layers_opacity=1,1,1,0.8&layers_visibility=false,false,false,false&layers_timestamp=18641231,,,",
+            "https://map.geo.admin.ch/?lang=fr&topic=ech&bgLayer=ch.swisstopo.pixelkarte-farbe&layers=ch.swisstopo.swisstlm3d-wanderwege&layers_opacity=0.8",
+            "https://map.geo.admin.ch/?lang=fr&topic=ech&bgLayer=ch.swisstopo.pixelkarte-farbe&layers=ch.swisstopo.swisstlm3d-wanderwege,ch.swisstopo.vec200-landcover,ch.bfs.arealstatistik-waldmischungsgrad,ch.bafu.biogeographische_regionen,ch.bfs.arealstatistik-bodenbedeckung-1985&layers_opacity=0.8,0.75,0.75,0.75,0.75&catalogNodes=457,477"
+        ]
+        self.invalid_urls_list = [
+            "https://map.geo.admin.ch/ThisIsAVeryLongTextWhoseGoalIsToMakeSureWeGoOverThe2046CharacterLimitOfTheServiceShortlinkUrl/ThisWillNowBeCopiedMultipleTimes/ThisIsAVeryLongTextWhoseGoalIsToMakeSureWeGoOverThe2046CharacterLimitOfTheServiceShortlinkUrl/ThisWillNowBeCopiedMultipleTimes/ThisIsAVeryLongTextWhoseGoalIsToMakeSureWeGoOverThe2046CharacterLimitOfTheServiceShortlinkUrl/ThisWillNowBeCopiedMultipleTimes/ThisIsAVeryLongTextWhoseGoalIsToMakeSureWeGoOverThe2046CharacterLimitOfTheServiceShortlinkUrl/ThisWillNowBeCopiedMultipleTimes/ThisIsAVeryLongTextWhoseGoalIsToMakeSureWeGoOverThe2046CharacterLimitOfTheServiceShortlinkUrl/ThisWillNowBeCopiedMultipleTimes/ThisIsAVeryLongTextWhoseGoalIsToMakeSureWeGoOverThe2046CharacterLimitOfTheServiceShortlinkUrl/ThisWillNowBeCopiedMultipleTimes/ThisIsAVeryLongTextWhoseGoalIsToMakeSureWeGoOverThe2046CharacterLimitOfTheServiceShortlinkUrl/ThisWillNowBeCopiedMultipleTimes/ThisIsAVeryLongTextWhoseGoalIsToMakeSureWeGoOverThe2046CharacterLimitOfTheServiceShortlinkUrl/ThisWillNowBeCopiedMultipleTimes/ThisIsAVeryLongTextWhoseGoalIsToMakeSureWeGoOverThe2046CharacterLimitOfTheServiceShortlinkUrl/ThisWillNowBeCopiedMultipleTimes/ThisIsAVeryLongTextWhoseGoalIsToMakeSureWeGoOverThe2046CharacterLimitOfTheServiceShortlinkUrl/ThisWillNowBeCopiedMultipleTimes/ThisIsAVeryLongTextWhoseGoalIsToMakeSureWeGoOverThe2046CharacterLimitOfTheServiceShortlinkUrl/ThisWillNowBeCopiedMultipleTimes/ThisIsAVeryLongTextWhoseGoalIsToMakeSureWeGoOverThe2046CharacterLimitOfTheServiceShortlinkUrl/ThisWillNowBeCopiedMultipleTimes/ThisIsAVeryLongTextWhoseGoalIsToMakeSureWeGoOverThe2046CharacterLimitOfTheServiceShortlinkUrl/ThisWillNowBeCopiedMultipleTimes/ThisIsAVeryLongTextWhoseGoalIsToMakeSureWeGoOverThe2046CharacterLimitOfTheServiceShortlinkUrl/ThisWillNowBeCopiedMultipleTimes/ThisIsAVeryLongTextWhoseGoalIsToMakeSureWeGoOverThe2046CharacterLimitOfTheServiceShortlinkUrl/ThisWillNowBeCopiedMultipleTimes/ThisIsAVeryLongTextWhoseGoalIsToMakeSureWeGoOverThe2046CharacterLimitOfTheServiceShortlinkUrl/ThisWillNowBeCopiedMultipleTimes/ThisIsAVeryLongTextWhoseGoalIsToMakeSureWeGoOverThe2046CharacterLimitOfTheServiceShortlinkUrl/ThisWillNowBeCopiedMultipleTimes/ThisIsAVeryLongTextWhoseGoalIsToMakeSureWeGoOverThe2046CharacterLimitOfTheServiceShortlinkUrl/ThisWillNowBeCopiedMultipleTimes/ThisIsAVeryLongTextWhoseGoalIsToMakeSureWeGoOverThe2046CharacterLimitOfTheServiceShortlinkUrl/ThisWillNowBeCopiedMultipleTimes/ThisIsAVeryLongTextWhoseGoalIsToMakeSureWeGoOverThe2046CharacterLimitOfTheServiceShortlinkUrl/ThisWillNowBeCopiedMultipleTimes/ThisIsAVeryLongTextWhoseGoalIsToMakeSureWeGoOverThe2046CharacterLimitOfTheServiceShortlinkUrl/ThisWillNowBeCopiedMultipleTimes/ThisIsAVeryLongTextWhoseGoalIsToMakeSureWeGoOverThe2046CharacterLimitOfTheServiceShortlinkUrl/ThisWillNowBeCopiedMultipleTimes/ThisIsAVeryLongTextWhoseGoalIsToMakeSureWeGoOverThe2046CharacterLimitOfTheServiceShortlinkUrl/ThisWillNowBeCopiedMultipleTimes/"
+        ]
+        self.uuid_to_url_dict = dict()
         os.environ['AWS_ACCESS_KEY_ID'] = 'testing'
         os.environ['AWS_SECRET_ACCESS_KEY'] = 'testing'
         os.environ['AWS_SECURITY_TOKEN'] = 'testing'
@@ -19,126 +33,75 @@ class TestDynamoDb(unittest.TestCase):
         logger.debug("Setting up Dynamo Db tests")
         region = 'eu-central-1'
         self.connection = boto3.resource('dynamodb', region)
-        # self.conn = boto3.resource('dynamodb', region_name=self.region)
-        self.keys_and_urls = {}
-        self.testing_urls = ["https://map.geo.admin"]
-        self.create_fake_table(self.connection)
-        self.table = self.connection.Table('shorturl')
-        logger.info(self.table)
-        self.populate_table()
-
-    @mock_dynamodb2
-    def populate_table(self):
-        from app.helpers import create_url
-        logger.warning("Populating the table using the create_url method")
-        logger.info(self.testing_urls)
-
-        for url in self.testing_urls:
-            logger.info(url)
-            logger.info(self.table)
-            """uuid_key = "10341324ef"
-            now = time.localtime()
-            self.table.put_item(
-                Item={
-                    'url_short': uuid_key,
-                    'url': url,
-                    'timestamp': time.strftime('%Y-%m-%d %X', now),
-                    'epoch': str(time.gmtime())
-                })"""
-            uuid_key = create_url(self.table, url)
-            self.keys_and_urls[uuid_key] = url
-        logger.warning(self.keys_and_urls)
-
-    @mock_dynamodb2
-    def create_fake_table(self, connection):
-
         logger.warning("Right before table creation")
-        connection.create_table(
+        self.connection.create_table(
             TableName='shorturl',
             AttributeDefinitions=[
-                {
-                    'AttributeName': 'url',
-                    'AttributeType': 'S'
-                },
-                {
-                    'AttributeName': 'url_short',
-                    'AttributeType': 'S'
-                },
-                {
-                    'AttributeName': 'timestamp',
-                    'AttributeType': 'S'
-                },
-                {
-                    'AttributeName': 'epoch',
-                    'AttributeType': 'S'
-                }
+                    {
+                        'AttributeName': 'url',
+                        'AttributeType': 'S'
+                    },
+                    {
+                        'AttributeName': 'url_short',
+                        'AttributeType': 'S'
+                    }
 
-            ],
-
+                ],
             KeySchema=[
                 {
-                    'AttributeName': 'url_short',
-                    'KeyType': 'HASH'
-                },
+                        'AttributeName': 'url_short',
+                        'KeyType': 'HASH'
+                    },
                 {
-                    'AttributeName': 'url',
-                    'KeyType': 'HASH'
-                },
-                {
-                    'AttributeName': 'timestamp',
-                    'KeyType': 'HASH'
-                },
-                {
-                    'AttributeName': 'epoch',
-                    'KeyType': 'HASH'
-                }
+                        'AttributeName': 'url',
+                        'KeyType': 'HASH'
+                    }
             ],
             LocalSecondaryIndexes=[
-                {
-                    'IndexName': 'URL',
-                    'KeySchema': [
-                        {
-                            'AttributeName': 'url',
-                            'KeyType': 'HASH'
+                    {
+                        'IndexName': 'URL',
+                        'KeySchema': [
+                            {
+                                'AttributeName': 'url',
+                                'KeyType': 'HASH'
+                            }
+                        ],
+                        'Projection': {
+                            'ProjectionType': 'INCLUDE',
+                            'NonKeyAttributes': ['url_short']
                         }
-                    ],
-                    'Projection': {
-                        'ProjectionType': 'INCLUDE',
-                        'NonKeyAttributes': ['url_short']
-                    }
-                },
-                {
-                    'IndexName': 'shorlinkID',
-                    'KeySchema': [
-                        {
-                            'AttributeName': 'url_short',
-                            'KeyType': 'HASH'
+                    },
+                    {
+                        'IndexName': 'shortlinkID',
+                        'KeySchema': [
+                            {
+                                'AttributeName': 'url_short',
+                                'KeyType': 'HASH'
+                            }
+                        ],
+                        'Projection': {
+                            'ProjectionType': 'INCLUDE',
+                            'NonKeyAttributes': ['url']
                         }
-                    ],
-                    'Projection': {
-                        'ProjectionType': 'INCLUDE',
-                        'NonKeyAttributes': ['url']
                     }
-                }
-            ],
+                ],
             ProvisionedThroughput={
-                'ReadCapacityUnits': 123,
-                'WriteCapacityUnits': 123
-            }
+                    'ReadCapacityUnits': 123,
+                    'WriteCapacityUnits': 123
+                }
         )
+        self.table = self.connection.Table('shorturl')
+        from app.helpers import create_url
+        for url in self.valid_urls_list:
+            uuid = (create_url(self.table, url))
+            self.uuid_to_url_dict[uuid] = url
 
+    @mock_dynamodb2
     def test_fetch_url(self):
+        self.setup()
         from app.helpers import fetch_url
-        logger.warning("in test_fetch_url")
-        self.setup()
-        for key, url in self.keys_and_urls.items():
-            assert (fetch_url(self.table, key) == url)
-
-    def test_check_and_get_url_short(self):
-        from app.helpers import check_and_get_url_short
-        self.setup()
-        for key, url in self.keys_and_urls.items():
-            assert (check_and_get_url_short(self.table, url) == key)
+        for uuid, url in self.uuid_to_url_dict.items():
+            self.assertEqual(fetch_url(self.table, uuid), url)
 
 
 if __name__ == '__main__':
