fb410095318570f0e746a585f15bc9f6420b5139
ltkum <kunzi.martin@gmail.com>
2020-11-05 16:13:24 +0100

    I'll have all possible linter error messages soon

--- app/routes.py
+++ app/routes.py
@@ -47,7 +47,7 @@ def checker():
     """
     # These multiple loggers are here to help me undestand which log syntax should be used
     logger.info("Checker route entered at %f", time.time())
-    logger.info("Checker route entered at %f",  time.time())
+    logger.info("Checker route entered at %f" % time.time())
     logger.info("Checker route entered at {time}".format(time=time.time()))
     logger.info(f"Checker route entered at {time.time()}")
     return make_response(jsonify({'success': True, 'message': 'OK'}))
