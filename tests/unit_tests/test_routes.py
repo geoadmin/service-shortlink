99ec6e2c1194551925e404b94e60de7857c7635f
Martin Künzi <kunzi.martin@gmail.com>
2020-11-10 11:24:27 +0100

    renaming route calls and using responses assertions in a correct fashion

--- tests/unit_tests/test_routes.py
+++ tests/unit_tests/test_routes.py
@@ -1,7 +1,10 @@
 import json
+import re
 import unittest
 from flask import make_response
 from flask import jsonify
+
+import service_config
 from app import app
 from app import routes
 
@@ -30,15 +33,18 @@ class TestRoutes(unittest.TestCase):
     def test_create_shortlink_ok(self):
         self.setUp()
         response = self.app.post(
-            f"/create_shortlink",
+            f"/shorten",
             data=json.dumps({"url": "https://map.geo.admin.ch/test"}),
             content_type="application/json",
             headers={"Origin": "map.geo.admin.ch"}
         )
+        print(response.json)
         self.assertEqual(response.status_code, 200)
         self.assertEqual(response.content_type, "application/json")
-        self.assertEqual(response.json, {'success': True, 'shorturl': ""})
-
+        # as the shorturl won't be known beforehand, we are not trying to check if it's equal to something
+        # only if it looks like what we expect
+        self.assertEqual(response.json.get('success'), True)
+        self.assertEqual(re.search(r"^\d{10}$", response.json.get('shorturl')) is not None, True)
 
     """
     The following tests should all return a 400 error code
@@ -46,54 +52,80 @@ class TestRoutes(unittest.TestCase):
     def test_create_shortlink_no_url(self):
         self.setUp()
         response = self.app.post(
-            f"/create_shortlink",
+            f"/shorten",
             content_type="application/json",
             headers={"Origin": "map.geo.admin.ch"}
         )
+        print(response)
         self.assertEqual(response.status_code, 400)
         self.assertEqual(response.content_type, "application/json")
-        self.assertEqual(response.json, {'success': True, 'shorturl': ""})
+        self.assertEqual(response.json, {
+            'success': False,
+            'error': {
+                'code': 400,
+                'message': 'No url given to shorten, exiting with a bad request'
+            }
+        })
 
     def test_create_shortlink_no_hostname(self):
         self.setUp()
         response = self.app.post(
-            f"/create_shortlink",
+            f"/shorten",
             data=json.dumps({"url": "/test"}),
             content_type="application/json",
             headers={"Origin": "map.geo.admin.ch"}
         )
         self.assertEqual(response.status_code, 400)
         self.assertEqual(response.content_type, "application/json")
-        self.assertEqual(response.json, {'success': True, 'shorturl': ""})
+        self.assertEqual(response.json, {
+            'success': False,
+            'error': {
+                'code': 400,
+                'message': 'Could not determine the query hostname'
+            }
+        })
 
     def test_create_shortlink_non_allowed_hostname_and_domain(self):
         self.setUp()
         response = self.app.post(
-            f"/create_shortlink",
+            f"/shorten",
             data=json.dumps({"url": "https://not.a.valid.hostname.ch/test"}),
             content_type="application/json",
             headers={"Origin": "map.geo.admin.ch"}
         )
         self.assertEqual(response.status_code, 400)
         self.assertEqual(response.content_type, "application/json")
-        self.assertEqual(response.json, {'success': True, 'shorturl': ""})
+        self.assertEqual(response.json, {
+            'success': False,
+            'error': {
+                'code': 400,
+                'message': 'Neither Host nor Domain in the url parameter are valid'
+            }
+        })
 
     def test_create_shortlink_url_too_long(self):
         self.setUp()
+        url = "https://map.geo.admin.ch/ThisIsAVeryLongTextWhoseGoalIsToMakeSureWeGoOverThe2046CharacterLimitOfTheServiceShortlinkUrl/ThisWillNowBeCopiedMultipleTimes/ThisIsAVeryLongTextWhoseGoalIsToMakeSureWeGoOverThe2046CharacterLimitOfTheServiceShortlinkUrl/ThisWillNowBeCopiedMultipleTimes/ThisIsAVeryLongTextWhoseGoalIsToMakeSureWeGoOverThe2046CharacterLimitOfTheServiceShortlinkUrl/ThisWillNowBeCopiedMultipleTimes/ThisIsAVeryLongTextWhoseGoalIsToMakeSureWeGoOverThe2046CharacterLimitOfTheServiceShortlinkUrl/ThisWillNowBeCopiedMultipleTimes/ThisIsAVeryLongTextWhoseGoalIsToMakeSureWeGoOverThe2046CharacterLimitOfTheServiceShortlinkUrl/ThisWillNowBeCopiedMultipleTimes/ThisIsAVeryLongTextWhoseGoalIsToMakeSureWeGoOverThe2046CharacterLimitOfTheServiceShortlinkUrl/ThisWillNowBeCopiedMultipleTimes/ThisIsAVeryLongTextWhoseGoalIsToMakeSureWeGoOverThe2046CharacterLimitOfTheServiceShortlinkUrl/ThisWillNowBeCopiedMultipleTimes/ThisIsAVeryLongTextWhoseGoalIsToMakeSureWeGoOverThe2046CharacterLimitOfTheServiceShortlinkUrl/ThisWillNowBeCopiedMultipleTimes/ThisIsAVeryLongTextWhoseGoalIsToMakeSureWeGoOverThe2046CharacterLimitOfTheServiceShortlinkUrl/ThisWillNowBeCopiedMultipleTimes/ThisIsAVeryLongTextWhoseGoalIsToMakeSureWeGoOverThe2046CharacterLimitOfTheServiceShortlinkUrl/ThisWillNowBeCopiedMultipleTimes/ThisIsAVeryLongTextWhoseGoalIsToMakeSureWeGoOverThe2046CharacterLimitOfTheServiceShortlinkUrl/ThisWillNowBeCopiedMultipleTimes/ThisIsAVeryLongTextWhoseGoalIsToMakeSureWeGoOverThe2046CharacterLimitOfTheServiceShortlinkUrl/ThisWillNowBeCopiedMultipleTimes/ThisIsAVeryLongTextWhoseGoalIsToMakeSureWeGoOverThe2046CharacterLimitOfTheServiceShortlinkUrl/ThisWillNowBeCopiedMultipleTimes/ThisIsAVeryLongTextWhoseGoalIsToMakeSureWeGoOverThe2046CharacterLimitOfTheServiceShortlinkUrl/ThisWillNowBeCopiedMultipleTimes/ThisIsAVeryLongTextWhoseGoalIsToMakeSureWeGoOverThe2046CharacterLimitOfTheServiceShortlinkUrl/ThisWillNowBeCopiedMultipleTimes/ThisIsAVeryLongTextWhoseGoalIsToMakeSureWeGoOverThe2046CharacterLimitOfTheServiceShortlinkUrl/ThisWillNowBeCopiedMultipleTimes/ThisIsAVeryLongTextWhoseGoalIsToMakeSureWeGoOverThe2046CharacterLimitOfTheServiceShortlinkUrl/ThisWillNowBeCopiedMultipleTimes/ThisIsAVeryLongTextWhoseGoalIsToMakeSureWeGoOverThe2046CharacterLimitOfTheServiceShortlinkUrl/ThisWillNowBeCopiedMultipleTimes/ThisIsAVeryLongTextWhoseGoalIsToMakeSureWeGoOverThe2046CharacterLimitOfTheServiceShortlinkUrl/ThisWillNowBeCopiedMultipleTimes/ThisIsAVeryLongTextWhoseGoalIsToMakeSureWeGoOverThe2046CharacterLimitOfTheServiceShortlinkUrl/ThisWillNowBeCopiedMultipleTimes/ThisIsAVeryLongTextWhoseGoalIsToMakeSureWeGoOverThe2046CharacterLimitOfTheServiceShortlinkUrl/ThisWillNowBeCopiedMultipleTimes/ThisIsAVeryLongTextWhoseGoalIsToMakeSureWeGoOverThe2046CharacterLimitOfTheServiceShortlinkUrl/ThisWillNowBeCopiedMultipleTimes/ThisIsAVeryLongTextWhoseGoalIsToMakeSureWeGoOverThe2046CharacterLimitOfTheServiceShortlinkUrl/ThisWillNowBeCopiedMultipleTimes/" # pylint: disable=line-too-long
         response = self.app.post(
-            f"/create_shortlink",
-            data=json.dumps({"url": "https://map.geo.admin.ch/ThisIsAVeryLongTextWhoseGoalIsToMakeSureWeGoOverThe2046CharacterLimitOfTheServiceShortlinkUrl/ThisWillNowBeCopiedMultipleTimes/ThisIsAVeryLongTextWhoseGoalIsToMakeSureWeGoOverThe2046CharacterLimitOfTheServiceShortlinkUrl/ThisWillNowBeCopiedMultipleTimes/ThisIsAVeryLongTextWhoseGoalIsToMakeSureWeGoOverThe2046CharacterLimitOfTheServiceShortlinkUrl/ThisWillNowBeCopiedMultipleTimes/ThisIsAVeryLongTextWhoseGoalIsToMakeSureWeGoOverThe2046CharacterLimitOfTheServiceShortlinkUrl/ThisWillNowBeCopiedMultipleTimes/ThisIsAVeryLongTextWhoseGoalIsToMakeSureWeGoOverThe2046CharacterLimitOfTheServiceShortlinkUrl/ThisWillNowBeCopiedMultipleTimes/ThisIsAVeryLongTextWhoseGoalIsToMakeSureWeGoOverThe2046CharacterLimitOfTheServiceShortlinkUrl/ThisWillNowBeCopiedMultipleTimes/ThisIsAVeryLongTextWhoseGoalIsToMakeSureWeGoOverThe2046CharacterLimitOfTheServiceShortlinkUrl/ThisWillNowBeCopiedMultipleTimes/ThisIsAVeryLongTextWhoseGoalIsToMakeSureWeGoOverThe2046CharacterLimitOfTheServiceShortlinkUrl/ThisWillNowBeCopiedMultipleTimes/ThisIsAVeryLongTextWhoseGoalIsToMakeSureWeGoOverThe2046CharacterLimitOfTheServiceShortlinkUrl/ThisWillNowBeCopiedMultipleTimes/ThisIsAVeryLongTextWhoseGoalIsToMakeSureWeGoOverThe2046CharacterLimitOfTheServiceShortlinkUrl/ThisWillNowBeCopiedMultipleTimes/ThisIsAVeryLongTextWhoseGoalIsToMakeSureWeGoOverThe2046CharacterLimitOfTheServiceShortlinkUrl/ThisWillNowBeCopiedMultipleTimes/ThisIsAVeryLongTextWhoseGoalIsToMakeSureWeGoOverThe2046CharacterLimitOfTheServiceShortlinkUrl/ThisWillNowBeCopiedMultipleTimes/ThisIsAVeryLongTextWhoseGoalIsToMakeSureWeGoOverThe2046CharacterLimitOfTheServiceShortlinkUrl/ThisWillNowBeCopiedMultipleTimes/ThisIsAVeryLongTextWhoseGoalIsToMakeSureWeGoOverThe2046CharacterLimitOfTheServiceShortlinkUrl/ThisWillNowBeCopiedMultipleTimes/ThisIsAVeryLongTextWhoseGoalIsToMakeSureWeGoOverThe2046CharacterLimitOfTheServiceShortlinkUrl/ThisWillNowBeCopiedMultipleTimes/ThisIsAVeryLongTextWhoseGoalIsToMakeSureWeGoOverThe2046CharacterLimitOfTheServiceShortlinkUrl/ThisWillNowBeCopiedMultipleTimes/ThisIsAVeryLongTextWhoseGoalIsToMakeSureWeGoOverThe2046CharacterLimitOfTheServiceShortlinkUrl/ThisWillNowBeCopiedMultipleTimes/ThisIsAVeryLongTextWhoseGoalIsToMakeSureWeGoOverThe2046CharacterLimitOfTheServiceShortlinkUrl/ThisWillNowBeCopiedMultipleTimes/ThisIsAVeryLongTextWhoseGoalIsToMakeSureWeGoOverThe2046CharacterLimitOfTheServiceShortlinkUrl/ThisWillNowBeCopiedMultipleTimes/ThisIsAVeryLongTextWhoseGoalIsToMakeSureWeGoOverThe2046CharacterLimitOfTheServiceShortlinkUrl/ThisWillNowBeCopiedMultipleTimes/ThisIsAVeryLongTextWhoseGoalIsToMakeSureWeGoOverThe2046CharacterLimitOfTheServiceShortlinkUrl/ThisWillNowBeCopiedMultipleTimes/ThisIsAVeryLongTextWhoseGoalIsToMakeSureWeGoOverThe2046CharacterLimitOfTheServiceShortlinkUrl/ThisWillNowBeCopiedMultipleTimes/ThisIsAVeryLongTextWhoseGoalIsToMakeSureWeGoOverThe2046CharacterLimitOfTheServiceShortlinkUrl/ThisWillNowBeCopiedMultipleTimes/"}),  # pylint: disable=line-too-long
+            f"/shorten",
+            data=json.dumps({"url": url}),
             content_type="application/json",
             headers={"Origin": "map.geo.admin.ch"}
         )
         self.assertEqual(response.status_code, 400)
         self.assertEqual(response.content_type, "application/json")
-        self.assertEqual(response.json, {'success': True, 'shorturl': ""})
+        self.assertEqual(response.json, {
+            'success': False,
+            'error': {
+                'code': 400,
+                'message': f"Url({url}) given as parameter exceeds characters limit."
+            }
+        })
 
     def test_redirect_shortlink_ok(self):
         self.setUp()
         response = self.app.get(
-            f"/redirect_shortlink/1578091241",
+            f"/redirect/1578091241",
             content_type="text/html",
             headers={"Origin": "map.geo.admin.ch"}
         )
@@ -105,7 +137,7 @@ class TestRoutes(unittest.TestCase):
     def test_redirect_shortlink_url_not_found(self):
         self.setUp()
         response = self.app.get(
-            f"/redirect_shortlink/nonexistent",
+            f"/redirect/nonexistent",
             content_type="text/html",
             headers={"Origin": "map.geo.admin.ch"}
         )
