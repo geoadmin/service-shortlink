014b13797fc90d0ee163fefca0007ffdafcb9641
Martin Künzi <kunzi.martin@gmail.com>
2020-11-16 07:55:14 +0100

    prefixing regex string with regex prefix

--- service_config.py
+++ service_config.py
@@ -12,4 +12,4 @@ class Config(object):
     allowed_hosts = os.environ.get('ALLOWED_HOSTS', 'api.geo.admin.ch,api3.geo.admin.ch').split(',')
     aws_table_name = os.environ.get('AWS_DYNAMODB_TABLE_NAME', 'shortlink')
     aws_region = os.environ.get('AWS_DYNAMODB_TABLE_REGION', 'eu-west-1')
-    allowed_domains_pattern = re.compile("^.*\.admin\.ch|.*\.swisstopo\.cloud|.*\.bgdi\.ch$")
+    allowed_domains_pattern = re.compile(r"^.*\.admin\.ch|.*\.swisstopo\.cloud|.*\.bgdi\.ch$")
