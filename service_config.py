04e9e42552faeb8dc3a89d888144e9271c5261ba
Martin Künzi <kunzi.martin@gmail.com>
2020-11-30 09:48:44 +0100

    tests are apparently using default value. It's now set to shortlinks_test

--- service_config.py
+++ service_config.py
@@ -8,6 +8,6 @@ value and an environment value to override it.
 """
 allowed_domains = os.environ.get('ALLOWED_DOMAINS', 'admin.ch,swisstopo.ch,bgdi.ch').split(',')
 allowed_hosts = os.environ.get('ALLOWED_HOSTS', 'api.geo.admin.ch,api3.geo.admin.ch').split(',')
-aws_table_name = os.environ.get('AWS_DYNAMODB_TABLE_NAME', 'shortlinks_dev')
+aws_table_name = os.environ.get('AWS_DYNAMODB_TABLE_NAME', 'shortlinks_test')
 aws_region = os.environ.get('AWS_DYNAMODB_TABLE_REGION', 'eu-central-1')
 allowed_domains_pattern = re.compile(r"^.*\.admin\.ch|.*\.swisstopo\.cloud|.*\.bgdi\.ch$")
